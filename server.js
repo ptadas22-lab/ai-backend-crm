aa02ea5c10a166305bd97ae416d40f01c0edd9bb
ptadas22-lab <ptadas22@gmail.com>
2026-05-17 21:58:49 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
-require("dotenv").config();
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+console.log("ENV CHECK:", process.env.GEMINI_API_KEY);
 const express = require("express");
 const cors = require("cors");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
